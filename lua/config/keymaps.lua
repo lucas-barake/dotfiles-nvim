afb4d62b73c6f92e7ae3be1ff83b2dbafc280036
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2026-01-29 23:15:59 -0500

    add path copy keymaps, slow scroll, disable swap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>fp", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, { desc = "Copy relative path" })
+
+vim.keymap.set("n", "<leader>fP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path)
+end, { desc = "Copy absolute path" })
+
+vim.keymap.set({ "n", "i", "v" }, "<ScrollWheelUp>", "<C-y>", { desc = "Scroll up (slow)" })
+vim.keymap.set({ "n", "i", "v" }, "<ScrollWheelDown>", "<C-e>", { desc = "Scroll down (slow)" })
