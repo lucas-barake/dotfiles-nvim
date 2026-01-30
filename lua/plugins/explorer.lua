47faa8975658545904b2096a7b074ed5ebbfebd2
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2026-01-30 09:56:10 -0500

    disable hidden and ignored files by default

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -4,14 +4,8 @@ return {
     opts = {
       picker = {
         sources = {
-          explorer = {
-            hidden = true,
-            ignored = true,
-          },
-          files = {
-            hidden = true,
-            ignored = true,
-          },
+          explorer = {},
+          files = {},
         },
       },
     },
